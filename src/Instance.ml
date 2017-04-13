193c8d7be7bfe4b70ad0fc1a87511c284319ea97
David Chemouil <david.chemouil+git@onera.fr>
2017-04-13 16:47:49 +0200

    git add Instance.ml[i]....

---
+++ src/Instance.ml
@@ -0,0 +1,33 @@
+open Containers
+
+module Map = Name.Map
+
+
+type t = TupleSet.t Map.t
+
+let empty = Map.empty
+
+let mem = Map.mem
+
+let add name rel ts =
+  assert (not @@ Map.mem name ts);
+  Map.add name rel ts
+
+let get_exn = Map.find
+
+let get = Map.get
+
+let to_list = Map.to_list
+
+
+let pp out rels =
+  let open Fmtc in
+  begin
+    (styled `Bold pf) out "inst@ ";
+    pf out "  %a"
+      (vbox @@ Map.pp ~sep:" " ~arrow:" = " ~start:"" ~stop:""
+                 (styled `Cyan Name.pp) TupleSet.pp) rels
+  end
+
+module P = Intf.Print.Mixin(struct type nonrec t = t let pp = pp end)
+include P 
---
+++ src/Instance.mli
@@ -0,0 +1,32 @@
+(** An instance is a set of relations whose value is a fixed tuple set. *)
+
+(** Virtually: a map between relation names and sets of tuples. *)
+type t 
+
+(** Constructor. *)
+val empty : t
+
+(** Adds an association to the instance. 
+    The name must not be in the instance already. *)
+val add : Name.t -> TupleSet.t -> t -> t
+
+(** Checks whether a name is already bound in the map. *)
+val mem : Name.t -> t -> bool
+
+(** {1 Accessors}*)
+
+(** May raise Not_found. {b IT MAY BE BETTER TO RETURN AN EXACT SCOPE OR EVEN 
+    A RELATION (TO BE DECIDED WHEN INSTANCES ARE USED IN THE TRANSLATION)} *)
+val get_exn : Name.t -> t -> TupleSet.t
+
+(** May rather return None. {b IT MAY BE BETTER TO RETURN AN EXACT SCOPE OR EVEN 
+    A RELATION (TO BE DECIDED WHEN INSTANCES ARE USED IN THE TRANSLATION)} *)
+val get : Name.t -> t -> TupleSet.t option
+
+(** Returns the map as an association list. {b IT MAY BE BETTER TO RETURN AN
+    EXACT SCOPE OR EVEN A RELATION (TO BE DECIDED WHEN INSTANCES ARE USED IN THE
+    TRANSLATION)} *)
+val to_list : t -> (Name.t * TupleSet.t) list
+
+include Intf.Print.S with type t := t
+
