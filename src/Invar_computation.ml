e49dfb6884afd22f8d034f2f0b0cde2cfd475e60
Julien Brunel <julien.brunel@onera.fr>
2024-05-24 11:40:51 +0200

    Invar_computation handling of integer ifthenelse

--- src/Invar_computation.ml
+++ src/Invar_computation.ml
@@ -219,7 +219,7 @@ class ['self] computer (elo : Elo.t) =
     method build_Lshift () = max_color_wiwt
     method build_Div () = max_color_wiwt
     method build_Big_int () = Fun.id
-    method build_AIte () = failwith ("TODO" ^ __LOC__)
+    method build_AIte () = fun cond_color _ _ -> cond_color
   end
 
 (* Computes the color (Invar, Static_prop, Init or Temporal) of an
